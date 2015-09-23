713d719a021b85ff2c136e2ea66cd6bac7b0fb8f
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2015-09-23 16:38:56 +0200

    media section & add crossweb

--- src/js/script.js
+++ src/js/script.js
@@ -11,12 +11,22 @@ $(document).ready(function(){
   var partnersUrl = "https://droidconpl.github.io/droidcon-2015-web/model/partners.json";
   $.getJSON(partnersUrl, function(data){
     $.each(data.partners, function(index, element){
-      $("#brackets").append(
+      $("section#partner-section .brackets").append(
         "<div class=\"bracket\"><a href=\"" + element.partnerUrl
         + "\" target=\"_blank\"><img src=\"" + element.partnerLogo
         + "\" alt=\"" + element.partnerName + "\" /></a></div>");
     }).error(function(data) {
     console.log("Error!");
+    });
+    
+    $.each(data.medias, function(index, element){
+      $("section#media-section .brackets").append(
+        "<div class=\"bracket\"><a href=\"" + element.partnerUrl
+        + "\" target=\"_blank\"><img src=\"" + element.partnerLogo
+        + "\" alt=\"" + element.partnerName + "\" /></a></div>");
+    }).error(function(data) {
+    console.log("Error!");
+    });
   });
-});
+
 });
