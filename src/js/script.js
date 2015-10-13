5e918a6b00a430f03d92cd9dada84f353d91cdd9
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2015-10-13 23:54:57 +0200

    remove shrinkable menu bar

--- src/js/script.js
+++ src/js/script.js
@@ -47,30 +47,4 @@ $(document).ready(function(){
     });
   });
 
-  $(function(){
-    $('.header_nav').data('size','big');
-  });
-
-  $(window).scroll(function(){
-    if($(document).scrollTop() > 0)
-  {
-      if($('.header_nav').data('size') == 'big')
-      {
-          $('.header_nav').data('size','small');
-          $('.header_nav').stop().animate({
-              height:'50px'
-          },600);
-      }
-  }
-  else
-    {
-      if($('.header_nav').data('size') == 'small')
-        {
-          $('.header_nav').data('size','big');
-          $('.header_nav').stop().animate({
-              height:'88px'
-          },600);
-        }
-    }
-  });
 });
